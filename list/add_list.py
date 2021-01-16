753c056617f503d2dd9c580951782524638631b7
wiraphat2711 <wiraphat.a@ku.th>
2021-01-16 12:37:31 +0700

    add 2 file (add,change) in list

---
+++ list/add_list.py
@@ -0,0 +1,8 @@
+# example:1
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+# example:2
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)

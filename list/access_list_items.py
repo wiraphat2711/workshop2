272839e64ac9014d03f69aedb62a8535d52ee094
wiraphat2711 <wiraphat.a@ku.th>
2021-01-16 12:09:59 +0700

    add new folder list add new file access_list

---
+++ list/access_list_items.py
@@ -0,0 +1,10 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[0])  # apple
+print(thislist[1])  # banana
+print(thislist[2])  # cherry
+
+print(thislist[-1])  # cherry
+print(thislist[-2])  # banana
+print(thislist[-3])  # apple
+
+print(thislist[1:])  # ['banana', 'cherry']

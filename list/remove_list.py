837ae22270ea96655754c82c0455dffa44851b00
wiraphat2711 <wiraphat.a@ku.th>
2021-01-16 21:28:21 +0700

    add 4 file in folder list

---
+++ list/remove_list.py
@@ -0,0 +1,20 @@
+# EXAMPLE1
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+# EXAMPLE2
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+# EXAMPLE3
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+# EXAMPLE4
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+# EXAMPLE5
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
---
+++ list/sort_list.py
@@ -0,0 +1,8 @@
+# EXAMPLE1
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+# EXAMPLE2
+thislist = [100, 50, 65, 82, 23]
+thislist.sort(reverse=True)
+print(thislist)

24cda37380ce660dd0424cdf6d27ce879ed4b36f
wiraphat2711 <wiraphat.a@ku.th>
2021-01-17 11:30:35 +0700

    add file exercise_set

---
+++ Dictionaries/access_dictionary.py
@@ -0,0 +1,2 @@
+# EX1
+thisdict = {"brabd": "ford", "model": "Mustang", "year": 1964}
--- Sets/Join_set.py
+++ Sets/Join_set.py
@@ -13,11 +13,11 @@ print(set1)  # {'c','a','d','e',b','f'}
 # EX3
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
-set1.intersection_update(set2)
+set1.intersection_update(set2)  # หาตัวที่เหมือนกัน
 print(set1)  # {'apple'}
 
 # EX4
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
-set1.symmetric_difference_update(set2)
+set1.symmetric_difference_update(set2)  # เอาค่าที่ไม่เหมือนกัน
 print(set1)  # {'google','banana','microsoft','cherry'}

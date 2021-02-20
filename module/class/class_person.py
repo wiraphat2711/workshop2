f4868acae02e51dc7b99301ad0cb958aaddc55f2
wiraphat2711 <wiraphat.a@ku.th>
2021-02-20 12:15:46 +0700

    update

--- module/class/class_person.py
+++ module/class/class_person.py
@@ -15,15 +15,3 @@ class Person:
 
     def get_into(self):
         return {"name": self.name, "age": self.age}
-
-
-name = "pipusana petgumpoom"
-age = 26
-address = "London"
-
-person = Person(name, age, address)
-
-print(person.get_name())
-print(person.get_age())
-print(person.get_address())
-print(person.get_into())
---
+++ module/class/run_class.py
@@ -0,0 +1,12 @@
+import class_person
+
+name = "pipusana petgumpoom"
+age = 26
+address = "London"
+
+person = class_person.Person(name, age, address)
+
+print(person.get_name())
+print(person.get_age())
+print(person.get_address())
+print(person.get_into())
